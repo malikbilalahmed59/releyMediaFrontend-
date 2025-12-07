77d2c40be43f7d6bc8cfe70a9295ff05700a9b18
malikbilalahmed59 <malikbilalahmed59@gmail.com>
2025-12-07 22:39:15 +0500

    Fixing api response time issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ToastProvider } from "@/components/ui/toast";
@@ -29,6 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Zendesk Widget */}
+        <Script
+          id="ze-snippet"
+          src="https://static.zdassets.com/ekr/snippet.js?key=4bd78553-bbaa-4205-9c01-0289760ebe84"
+          strategy="afterInteractive"
+        />
         <AuthProvider>
           <ToastProvider>
             {children}
